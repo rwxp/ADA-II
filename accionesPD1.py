ed6658d29c1fbf66f33cc0b260800c10c24d8532
lauramoyano <laura.moyano.go@gmail.com>
2023-04-26 15:54:07 -0500

    documentation improved

--- accionesPD1.py
+++ accionesPD1.py
@@ -1,29 +1,42 @@
 import os
 
 def accionesPD1(A, B, n, offers):
+    #inicialización de la matriz que aloja los resultados para cada subproblema
     dp = [[-float('inf') for _ in range(A + 1)] for _ in range(n + 1)]
     dp[0][0] = 0
+    #recorre cada oferta del arreglo
     for i in range(1, n + 1):
         p, c, r = offers[i - 1]
-        for j in range(A + 1):
+        #para cada oferta recorre cada posible asignación de A
+        for j in range(A + 1): 
             dp[i][j] = dp[i - 1][j]
+            # k va desde el minimo de acciones a comprar por cada i y va hasta el min entre el 
+            # max de acciones a comprar "c" y "j"
             for k in range(r, min(c, j) + 1):
+                #calcular el max entre la solución óptima anterior 
+                # y la solución actual si se venden k acciones
                 dp[i][j] = max(dp[i][j], dp[i - 1][j - k] + k * p)
+    #array que guarda el resultado final de asignaciones
     result = [0] * n
     i = n
     j = A
+    #while que va desde 0 < i < n y 0 < j < A O(A*n)
     while i > 0 and j > 0:
+        #si la solución actual no cambia respecto a la anterior
         if dp[i][j] == dp[i - 1][j]:
             i -= 1
         else:
             p, c, r = offers[i - 1]
             k = min(c, j)
+            #escogemos un k que representa el num de acciones que cumple 
+            # con la restricción de min y hace parte de la solución óptima
             while k >= r and dp[i][j] != dp[i - 1][j - k] + k * p:
                 k -= 1
             result[i - 1] = k
             j -= k
             i -= 1
     costo = 0 
+    #calculamos el vr de la solución óptima
     for x in range(0, len(result)):
         costo += result[x]*offers[x][0]
     
