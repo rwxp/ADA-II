2580582da3fb40e91b04617547ab77dda875aa67
Juan Esteban <juan.betancourt.narvaez@correounivalle.edu.co>
2023-04-27 20:22:35 -0500

    Funcion para que el programa no se cierre si se abre una entrada que no es

--- main.py
+++ main.py
@@ -33,42 +33,54 @@ while True:
                 n  = int(lines[2]) 
                 offers = lines[3:]
                 offers = [tuple(int(num) for num in line[:-1].split(',') if num) for line in offers]
-            window['-INPUT-'].update(value=f'A={A}\nB={B}\nn={n}\nOfertas:\n{offers}')
-        else:
-            window['-INPUT-'].update(value='No se seleccionó archivo de entrada')
-
-    if event == 'Calcular':
-        file_path = values['-FILE-']
-        with open(file_path, 'r') as f:
-            lines = f.readlines()
-            A = int(lines[0])
-            B = int(lines[1])
-            n  = int(lines[2]) 
-            offers = lines[3:]
-            offers = [tuple(int(num) for num in line[:-1].split(',') if num) for line in offers]
-            
-            # Actualiza el area de texto de entrada
-            window['-INPUT-'].update(value=f'A={A}\nB={B}\nn={n}\nOfertas:\n{offers}')
-            
-            start = time.time()
-            if values['function'] == 'Programación dinámica 1':
-                result = accionesPD1(A,B, n, offers)
-            elif values['function'] == 'Programación dinámica 2':
-                if(len(offers) > n+1):
+                              
+                if(len(offers[-1]) == 1):
                     M = int(lines[-1])
                     offers.pop()
                 else:
                     M = 1
-                result = accionesPD2(A,B, n, M, offers)
-            elif values['function'] == 'Fuerza Bruta':
-                result = accionesFB(A,B,n, offers)
-            elif values['function'] == 'Programación Voraz':
-                result = accionesVR(A,B,n, offers)
-            
-            end = time.time()
-            window['-TIME-'].update(value=f'Tiempo de ejecución: {end - start} segundos')
-            # Actualiza el area de texto de salida
-            window['-OUTPUT-'].update(value=result)
+
+            window['-INPUT-'].update(value=f'A={A}\nB={B}\nn={n}\nOfertas:\n{offers}')
+        else:
+            window['-INPUT-'].update(value='No se seleccionó archivo de entrada')
+
+
+    if event == 'Calcular':
+        file_path = values['-FILE-']
+        if file_path:
+            with open(file_path, 'r') as f:
+                lines = f.readlines()
+                A = int(lines[0])
+                B = int(lines[1])
+                n  = int(lines[2]) 
+                offers = lines[3:]
+                offers = [tuple(int(num) for num in line[:-1].split(',') if num) for line in offers]
+                
+                if(len(offers[-1]) == 1):
+                    M = int(lines[-1])
+                    offers.pop()
+                else:
+                    M = 1
+                
+                # Actualiza el area de texto de entrada
+                window['-INPUT-'].update(value=f'A={A}\nB={B}\nn={n}\nOfertas:\n{offers}')
+                
+                start = time.time()
+                if values['function'] == 'Programación dinámica 1':
+                    result = accionesPD1(A,B, n, offers)
+                elif values['function'] == 'Programación dinámica 2':
+                    result = accionesPD2(A,B, n, M, offers)
+                elif values['function'] == 'Fuerza Bruta':
+                    result = accionesFB(A,B,n, offers)
+                elif values['function'] == 'Programación Voraz':
+                    result = accionesVR(A,B,n, offers)
+                
+                end = time.time()
+                window['-TIME-'].update(value=f'Tiempo de ejecución: {end - start} segundos')
+                # Actualiza el area de texto de salida
+                window['-OUTPUT-'].update(value=result)
+        else:
+            window['-INPUT-'].update(value='No se seleccionó archivo de entrada')
             
     if event == sg.WIN_CLOSED:
         break
